6f6feae395577825aff5ef56375e2d1a108443a7
Yongxue <chandler.yx.chen@gmail.com>
2026-07-28 14:14:10 +0100

    Fix visual test server path

--- test/visual/playwright.config.js
+++ test/visual/playwright.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("@playwright/test");
+const path = require("path");
 
 module.exports = defineConfig({
   testDir: __dirname,
@@ -9,6 +10,7 @@ module.exports = defineConfig({
   },
   webServer: {
     command: "python3 -m http.server 4173 --bind 127.0.0.1 --directory _site",
+    cwd: path.resolve(__dirname, "../.."),
     url: "http://127.0.0.1:4173",
     reuseExistingServer: true,
   },
